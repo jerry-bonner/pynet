6071241040b3b4884e1bb24b8416da494be16589
Applied Python user <jerry.bonner@gmail.com>
2017-09-24 08:48:26 -0700

    class 3 updates

---
+++ class3/ex1.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import snmp_helper
+from datetime import datetime
+import time
+import json
+import os.path
+import email_helper
+
+rtr = ('184.105.247.71', 161)
+snmp_creds = ('pysnmp', 'galileo1', 'galileo1')
+oids = {'RunningLastChanged':'1.3.6.1.4.1.9.9.43.1.1.1.0'}
+save_file = 'last_change.json'
+alert_address = 'jerry.bonner@gmail.com'
+
+def get_config_change(router_port, snmp_creds):
+    snmp_data  = snmp_helper.snmp_get_oid_v3(router_port, snmp_creds, oids['RunningLastChanged'])
+    return snmp_helper.snmp_extract(snmp_data)
+
+def get_last_config_change(filename):
+    # check if file exists
+    if (os.path.isfile(filename)):
+        f = open(filename, 'r')
+        data = json.load(f)
+        return data['last_change']
+    else:
+        return 0 
+
+def dump_last_config_change(last_change, filename):
+    data = {'last_change' : last_change}
+
+    with open(filename, 'w') as f:
+        json.dump(data, f)
+
+def send_email_alert(email, last_change, current_change):
+    
+    subject = "Config Change Alert!"
+    message = "Last change %s / New Change %s" % (last_change, current_change)
+
+    email_helper.send_mail(email, subject, message, 'test@twb-tech.com') 
+    
+if __name__ == "__main__":
+    last_change = get_last_config_change(save_file)
+    current_change = get_config_change(rtr, snmp_creds)
+
+    # check if we've ever stored the last_change before, must be > 0
+    if (last_change > 0):
+
+        # has the config changed?
+        if (last_change != current_change):
+            dump_last_config_change(current_change, save_file) 
+            send_email_alert(alert_address, last_change, current_change)
+    else:
+        # store config change for the first time
+        dump_last_config_change(current_change, save_file)
---
+++ class3/ex2.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+
+import pygal
+import time
+import snmp_helper
+
+rtr = ('184.105.247.71', 161)
+snmp_creds = ('pysnmp', 'galileo1', 'galileo1')
+
+alert_address = 'jerry.bonner@gmail.com'
+
+descr_oid = '1.3.6.1.2.1.2.2.1.2.5'
+
+oct_oids = {'ifInOctets_fa4':'1.3.6.1.2.1.2.2.1.10.5',
+    'ifOutOctets_fa4':'1.3.6.1.2.1.2.2.1.16.5'}
+
+oct_graph_file = "fa4_oct.svg"
+
+pkt_oids = {'ifInUcastPkts_fa4':'1.3.6.1.2.1.2.2.1.11.5',
+    'ifOutUcastPkts_fa4':'1.3.6.1.2.1.2.2.1.17.5'}
+
+pkt_graph_file = "fa4_pkt.svg"
+
+# Graph every x seconds
+sample_time = 3
+
+#-----------
+
+def poll_snmp_oid(router_port, snmp_creds, oid):
+    snmp_data = snmp_helper.snmp_get_oid_v3(router_port, snmp_creds, oid)
+    return snmp_helper.snmp_extract(snmp_data)
+
+def graph_snmp_data(xdata, sample_time, title, filename):
+
+    # Create a Chart of type Line
+    line_chart = pygal.Line()
+
+    # Title
+    line_chart.title = title
+
+    # create x axis data based on num of samples * sample_time
+
+    x_labels = []
+    
+    # get length of first item in xdata dict
+    for i in len(xdata.iteritems().next()):
+       x_labels.append(sample_time * i)
+ 
+    line_chart.x_labels = x_labels
+
+    # Add each one of the above lists into the graph as a line with corresponding label
+
+    for mib,data in xdata.iteritems():
+        print mib
+        print data
+        #line_chart.add('InPackets', fa4_in_packets)
+        line_chart.add(mib, data)
+
+    # Create an output image file from this
+    line_chart.render_to_file(filename)
+ 
+if __name__ == "__main__":
+
+    ifDesc = poll_snmp_oid(rtr, snmp_creds, descr_oid)
+
+    data = {}
+
+    # poll 12 times 
+    t = 12 
+
+    # initialize dicts
+
+    oct_data = {}
+    pkt_data = {}
+
+    for name,oid in oct_oids.iteritems():
+        oct_data[name] = []  
+
+    for name,oid in pkt_oids.iteritems():
+        pkt_data[name] = []  
+
+    # loop and collect data in lists
+
+    while t>1:
+
+        for name,oid in oct_oids.iteritems(): 
+            snmp_data = poll_snmp_oid(rtr, snmp_creds, oid)
+            oct_data[name].append(snmp_data)
+
+        for name,oid in pkt_oids.iteritems(): 
+            snmp_data = poll_snmp_oid(rtr, snmp_creds, oid)
+            pkt_data[name].append(snmp_data)
+
+        # graph raw snmp data to file
+
+        graph_snmp_data(oct_data, sample_time, ifDescr + " Bytes",   oct_graph_file)
+        graph_snmp_data(pkt_data, sample_time, ifDescr + " Packets", pkt_graph_file)
+
+        t = t-1
+
+        time.sleep(sample_time) 
