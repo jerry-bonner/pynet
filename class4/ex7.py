764ec4201c90324f00062f069f49e8536eac4d51
Applied Python user <jerry.bonner@gmail.com>
2017-10-26 11:18:14 -0700

    Fixed exersize 7 yet again.

--- class4/ex7.py
+++ class4/ex7.py
@@ -5,7 +5,10 @@ from my_devices import rtrs
 
 def main():
     conn = ConnectHandler(**rtrs['pynet-rtr2'])
-    conn.send_config_set("ip logging buffered 10000")
+    output = conn.send_config_set("logging buffered 10002")
+
+    print output
+
     return
  
 if __name__ == "__main__":
